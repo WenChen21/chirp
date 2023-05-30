232281a76c6fc73941fb97716442b9cca3676b91
Wen Chen <wc467@cornell.edu>
2023-05-30 15:01:32 -0400

    Next image tag, style up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,49 @@
-import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignIn, SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { api } from "npm/utils/api";
+import type { RouterOutputs } from "npm/utils/api";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from 'next/image';
 const CreatePostWizard = () => {
   const { user } = useUser();
+  console.log(user);
   if (!user) return null;
 
   return (
     <div className="flex gap-3 w-full">
-      <img src={user.profileImageUrl} alt="profile image" className="w-16 h-16 rounded-full" />
+      <Image
+        src={user.profileImageUrl}
+        alt="P rofile Image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
       <input placeholder="Type some emojis!" className="bg-transparent grow outline-none" />
     </div>)
 }
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 
-const PostView = () => {
-
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="flex border-b border-slate-400 p-4 gap-3">
+      <Image
+        src={author.profileImageUrl}
+        className="h-14 w-14 rounded-full"
+        alt={`@${author.username}'s profile picture`}
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex text-slate-300">
+          <span> {`@${author.username}`} </span>
+          <span className="font-thin"> {` . ${dayjs(post.createdAt).fromNow()}`}</span>  </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
+  )
 }
 const Home: NextPage = () => {
 
@@ -38,9 +66,9 @@ const Home: NextPage = () => {
           </div>
           <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
           <div className="flex flex-col">
-            {[...data, ...data]?.map(({ post, author }) => (
-              <div key={post.id} className="border-b border-white-100 p-8">
-                {post.content}</div>))}
+            {[...data, ...data]?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
+            ))}
           </div>
         </div>
       </main>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "npm/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -14,16 +15,28 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
-
     });
+
     const users = (await clerkClient.users.getUserList({
       userId: posts.map((post) => post.authorId),
       limit: 100,
-    })).map(filterUserForClient);
 
-    return posts.map((post) => ({
-      post,
-      author: users.find((user) => user.id === post.authorId),
-    }));
+    })).map(filterUserForClient);
+    console.log(users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId)
+      if (!author || !author.username) throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found",
+      });
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      }
+    });
   }),
 });
