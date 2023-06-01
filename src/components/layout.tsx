e4684174043c7ebccb5d8d0a2bb5d0c44236a0c7
Wen Chen <wc467@cornell.edu>
2023-06-01 09:47:27 -0400

    fixed layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,12 @@
 import type { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren) => {
   return (
-
     < main className="flex h-screen justify-center" >
       <div className="w-full md:max-w-2xl border-x h-full border-slate-400 overflow-y-scroll">
 
         {props.children}
       </div >
     </main >
-  )
-}
+  );
+};
